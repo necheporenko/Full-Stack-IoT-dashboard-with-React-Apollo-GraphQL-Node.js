bca1c899ca84d33bd3097eca724152c43805d012
necheporenko <ne4eporenko.v@gmail.com>
2019-02-13 16:43:11 +0200

    feat: update docker

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,7 +1,6 @@
-export const env = process.env.NODE_ENV || 'development';
+export const env = process.env.NODE_ENV || "development";
 export const SERVER_PORT = parseInt(process.env.PORT, 10) || 4000;
-export const corsDomain = process.env.CORS_DOMAIN || '*';
-export const DB_HOST = 'localhost';
+export const corsDomain = process.env.CORS_DOMAIN || "*";
+export const DB_HOST = "mongo";
 export const DB_PORT = 27017;
-export const DB_NAME = 'qraphql';
-
+export const DB_NAME = "graphqlDB";
--- server/src/server.js
+++ server/src/server.js
@@ -1,43 +1,64 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import { ApolloServer } from 'apollo-server-express';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import { ApolloServer } from "apollo-server-express";
 // Connect files from folders config, resolves, typeDefs
-import { SERVER_PORT, corsDomain, DB_HOST, DB_PORT, DB_NAME } from './config';
-import typeDefs from './typeDefs';
-import resolvers from './resolvers';
+import { SERVER_PORT, corsDomain, DB_HOST, DB_PORT, DB_NAME } from "./config";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
 
 const { ObjectId } = mongoose.Types;
-ObjectId.prototype.valueOf = function () {
-  return this.toString();  // Bring the ObjectId type to the String type
+ObjectId.prototype.valueOf = function() {
+  return this.toString(); // Bring the ObjectId type to the String type
 };
 
-// Connect to a database
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, { useNewUrlParser: true });
-mongoose.Promise = global.Promise;
-const db = mongoose.connection;
-db.once('open', () => console.log('✔️  MongoDB: Connection succeeded'));
-db.on('error', () => { throw new Error('❌  MongoDB: Unable to connect') });
+const options = {
+  autoIndex: false, // Don't build indexes
+  reconnectTries: 30, // Retry up to 30 times
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  useNewUrlParser: true
+};
+
+const connectWithRetry = () => {
+  console.log("🚀  MongoDB: Start connection");
+  mongoose
+    .connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, options)
+    .then(() => {
+      console.log("✔️  MongoDB: Connection succeeded");
+    })
+    .catch(err => {
+      console.log("❌  MongoDB: Unable to connect, retry after 5 seconds.");
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
 const app = express();
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(cors({
-  origin: corsDomain,
-  optionsSuccessStatus: 200
-}));
+app.use(
+  cors({
+    origin: corsDomain,
+    optionsSuccessStatus: 200
+  })
+);
 // Connect the contents of the folders resolves and typeDefs to the server
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
+  resolvers
 });
 // Installing all middlewares
-server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+  console.log("Hello from the console!");
+});
 
 app.listen(SERVER_PORT, () => console.log(`🚀  Server listening on port: ${SERVER_PORT}`));
-
-
