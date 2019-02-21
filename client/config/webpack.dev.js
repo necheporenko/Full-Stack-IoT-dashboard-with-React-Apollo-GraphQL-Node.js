4a748ed3c99f52c75b412cd05c7e601e19a0cab7
necheporenko <ne4eporenko.v@gmail.com>
2019-02-21 12:29:52 +0200

    feat: docker-compose

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -1,39 +1,32 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        port: 3002,
-        historyApiFallback: true,
-        overlay: true,
-        open: true,
-        stats: 'errors-only'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader?sourceMap" }
-                ]
-            },
-            {
-                test: /\.s(a|c)ss$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader" }
-                ]
-            },
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].[hash:8].css",
-        }),
-    ],
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    overlay: true,
+    open: true,
+    stats: 'errors-only'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader?sourceMap' }]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash:8].css'
+    })
+  ]
 });
